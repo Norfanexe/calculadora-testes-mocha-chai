da491396b1cca750e0a47337add87d2e371a0be3
2225198@aluno.univesp.br <2225198@aluno.univesp.br>
2025-09-07 23:29:58 -0300

    Chore: adicionado ESlint e configurado análise do projeto

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,41 +1,41 @@
 // Exporta a classe Calculadora para uso em outros módulos
 export default class Calculadora {
     
-    static soma(a, b) {
-        return a + b;
-    }
+  static soma(a, b) {
+    return a + b;
+  }
 
-    static subtracao(a, b) {
-        return a - b;
-    }
+  static subtracao(a, b) {
+    return a - b;
+  }
 
-    static multiplicacao(a, b) {
-        return a * b;
-    }
+  static multiplicacao(a, b) {
+    return a * b;
+  }
 
-    static divisao(a, b) {
-        if (b === 0) {
-            throw new Error("não é permitido divisões por zero");
-        }
-        return a / b;
+  static divisao(a, b) {
+    if (b === 0) {
+      throw new Error("não é permitido divisões por zero");
     }
+    return a / b;
+  }
 
-    static modulo(a, b) {
-        if (b === 0) {
-            throw new Error("não é permitido divisões por zero");
-        }
-        return a % b; // Refatorado para usar operador nativo
+  static modulo(a, b) {
+    if (b === 0) {
+      throw new Error("não é permitido divisões por zero");
     }
+    return a % b; // Refatorado para usar operador nativo
+  }
 
-    static fatorial(n) {
-        if (n < 0) {
-            throw new Error("não é permitido calcular o fatorial de um número negativo");
-        }
-        let resultado = 1;
-        for (let i = 1; i <= n; i++) {
-            resultado *= i;
-        }
-        return resultado;
+  static fatorial(n) {
+    if (n < 0) {
+      throw new Error("não é permitido calcular o fatorial de um número negativo");
     }
+    let resultado = 1;
+    for (let i = 1; i <= n; i++) {
+      resultado *= i;
+    }
+    return resultado;
+  }
 }
 
